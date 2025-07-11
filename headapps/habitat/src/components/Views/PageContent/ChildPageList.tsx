aa42055cf781fa03fa1bece5be39ac6a20899ce7
Martin Miles <miles@martin.by>
2025-07-11 06:47:18 -0700

    Added dictionary to ChildPageList

--- headapps/habitat/src/components/Views/PageContent/ChildPageList.tsx
+++ headapps/habitat/src/components/Views/PageContent/ChildPageList.tsx
@@ -1,23 +1,19 @@
 ﻿import { RichText } from '@sitecore-jss/sitecore-jss-nextjs';
 import React from 'react';
-import {
-  Image,
-  // LinkField,
-  // RichText,
-  ImageField,
-  Field,
-  // LinkField,
-} from '@sitecore-jss/sitecore-jss-nextjs';
+import { Image, ImageField, Field } from '@sitecore-jss/sitecore-jss-nextjs';
+import { useI18n } from 'next-localization';
 
-// interface ComponentProps {
-//   rendering: ComponentRendering & { params: ComponentParams };
-//   params: ComponentParams;
-// }
+export function useTranslations() {
+  const i18n = useI18n();
+  return {
+    readLabel: i18n?.t('Read more'),
+  };
+}
 
 interface ItemProps {
+  url: string;
   fields: {
     id: string;
-    url: string;
     Title: Field<string>;
     Summary: { value: string };
     Image: { value: ImageField };
@@ -31,26 +27,23 @@ interface PageFieldsProps {
 }
 
 const ChildPageList = (props: PageFieldsProps): JSX.Element => {
+  const translations = useTranslations();
   return (
     <>
       <h3>Child Page List</h3>
       <div className="block-grid-md-2 block-grid-sm-1 block-grid-xs-1">
         {props.fields.items.map((item, idx) => (
           <div key={idx} className="block-grid-item">
-            <div>
-              <div className="thumbnail">
-                <a href={item.fields.url}>
-                  <Image field={item.fields.Image} />
+            <div className="thumbnail">
+              <a href={item.url} target="_blank" rel="noopener noreferrer">
+                <Image field={item.fields.Image} />
+              </a>
+              <div className="caption">
+                <h3 className="teaser-heading">{item.fields.Title?.value}</h3>
+                <RichText field={item.fields.Summary} />
+                <a href={item.url} className="btn btn-default">
+                  {translations.readLabel}
                 </a>
-                <div className="caption">
-                  <h3 className="teaser-heading">{item.fields.Title?.value}</h3>
-
-                  <RichText field={item.fields.Summary} />
-
-                  <a href={item.fields.url} className="btn btn-default">
-                    Read more
-                  </a>
-                </div>
               </div>
             </div>
           </div>
