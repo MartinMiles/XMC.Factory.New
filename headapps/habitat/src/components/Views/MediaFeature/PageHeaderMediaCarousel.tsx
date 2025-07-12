946f04131187b4752c715ce6fd2754b67c3070a5
Martin Miles <miles@martin.by>
2025-07-12 01:33:05 -0700

    PageHeaderMediaCarousel component done, including video (hardcoded)

--- headapps/habitat/src/components/Views/MediaFeature/PageHeaderMediaCarousel.tsx
+++ headapps/habitat/src/components/Views/MediaFeature/PageHeaderMediaCarousel.tsx
@@ -1,16 +1,44 @@
-﻿import {
-  ComponentParams,
-  ComponentRendering,
-  Placeholder,
-} from '@sitecore-jss/sitecore-jss-nextjs';
+﻿import { Text, ImageField } from '@sitecore-jss/sitecore-jss-nextjs';
 import React from 'react';
 
-interface ComponentProps {
-  rendering: ComponentRendering & { params: ComponentParams };
-  params: ComponentParams;
+interface Field {
+  url: string;
+  fields: {
+    MediaImage: {
+      value: {
+        src: string;
+      };
+    };
+    MediaVideoLink: {
+      value: {
+        src: string;
+      };
+    };
+    MediaThumbnail: { value: ImageField };
+    MediaTitle: { value: string };
+    MediaDescription: { value: string };
+  };
 }
 
-const PageHeaderMediaCarousel = (props: ComponentProps): JSX.Element => {
+interface ReferencedItem {
+  jsonValue: Field[];
+}
+
+type PageHeaderMediaCarouselProps = {
+  params: { [key: string]: string };
+  fields: {
+    data: {
+      datasource: {
+        fields: ReferencedItem[];
+      };
+    };
+  };
+};
+
+const PageHeaderMediaCarousel = (props: PageHeaderMediaCarouselProps): JSX.Element => {
+  function isNonEmptyArray(val) {
+    return Array.isArray(val) && val.length > 0;
+  }
   return (
     <>
       <h3 style={{ color: 'red', margin: '10px' }}>Page Header Carousuel</h3>
@@ -19,103 +47,62 @@ const PageHeaderMediaCarousel = (props: ComponentProps): JSX.Element => {
         className="carousel slide"
         data-ride="carousel"
       >
-        <ol className="carousel-indicators">
-          <li
-            data-target="#carousela3acf7a2624c4483b85d2de0c451ab6f"
-            data-slide-to="0"
-            className="active"
-          ></li>
-          <li
-            data-target="#carousela3acf7a2624c4483b85d2de0c451ab6f"
-            data-slide-to="1"
-            className=""
-          ></li>
-          <li
-            data-target="#carousela3acf7a2624c4483b85d2de0c451ab6f"
-            data-slide-to="2"
-            className=""
-          ></li>
-          <li
-            data-target="#carousela3acf7a2624c4483b85d2de0c451ab6f"
-            data-slide-to="3"
-            className=""
-          ></li>
-        </ol>
-        <div className="carousel-inner" role="listbox">
-          <div className="item active">
-            <div
-              className="jumbotron jumbotron-xl bg-media"
-              style={{
-                backgroundImage: "url('/-/media/Habitat/Images/Wide/Habitat-004-wide.jpg')"
-              }}
-            >
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-12">
-                    <h1>Simplicity</h1>
-                    <p className="lead">A consistent and discoverable architecture</p>
+        {props.fields.data.datasource.fields.map(
+          (ref, idx) =>
+            isNonEmptyArray(ref?.jsonValue) && (
+              <ol key={idx} className="carousel-indicators">
+                {ref.jsonValue.map((item, index) => (
+                  <li
+                    key={index}
+                    data-target="#carousel{index}"
+                    data-slide-to={index}
+                    className={index === 0 ? 'active' : ''}
+                  ></li>
+                ))}
+              </ol>
+            )
+        )}
+        {props.fields.data.datasource.fields.map(
+          (ref, idx) =>
+            isNonEmptyArray(ref?.jsonValue) && (
+              <div key={idx} className="carousel-inner" role="listbox">
+                {ref.jsonValue.map((item, index) => (
+                  <div key={index} className="item active">
+                    <div
+                      className="jumbotron jumbotron-xl bg-media"
+                      style={{
+                        backgroundImage: `url('${item.fields.MediaThumbnail.value.src}')`,
+                      }}
+                    >
+                      {item.fields.MediaVideoLink &&
+                        item.fields.MediaVideoLink.value &&
+                        (() => (
+                          <video autoPlay={true} loop={true} muted={true} className="video-bg">
+                            <source
+                              src="https://xmcloudcm.localhost/-/media/Habitat/Videos/Sitecore-Experience.mp4"
+                              type="video/mp4"
+                            ></source>
+                          </video>
+                        ))()}
+                      <div className="container">
+                        <div className="row">
+                          <div className="col-md-12">
+                            <h1>
+                              <Text field={item.fields.MediaTitle} />
+                            </h1>
+                            <p className="lead">
+                              <Text field={item.fields.MediaDescription} />
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                </div>
+                ))}
               </div>
-            </div>
-          </div>
-          <div className="item ">
-            <div
-              className="jumbotron jumbotron-xl bg-media"
-              style={{
-                backgroundImage: "url('/-/media/Habitat/Images/Wide/Habitat-007-wide.jpg')"
-              }}
-            >
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-12">
-                    <h1>Flexibility</h1>
-                    <p className="lead">Change and add quickly and without worry</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="item ">
-            <div
-              className="jumbotron jumbotron-xl bg-media"
-              style={{
-                backgroundImage: "url('/-/media/Habitat/Images/Wide/Habitat-001-wide.jpg')"
-              }}
-            >
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-12">
-                    <h1>Extensibility</h1>
-                    <p className="lead">
-                      Add new features, simply and without a steep learning curve
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="item ">
-            <div
-              className="jumbotron jumbotron-xl bg-media"
-              style={{
-                backgroundImage: "url('/-/media/Habitat/Images/Wide/Habitat-071-wide.jpg')",
-              }}
-            >
-              <video autoPlay={true} loop={true} muted={true} className="video-bg">
-                <source src="/-/media/Habitat/Videos/Sitecore-Experience.mp4" type="video/mp4" />
-              </video>
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-12">
-                    <h1>Sitecore Powered!</h1>
-                    <p className="lead">Fully Leveraging the power of Sitecore</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+            )
+        )}
+
         <a
           className="left carousel-control"
           href="#carousela3acf7a2624c4483b85d2de0c451ab6f"
